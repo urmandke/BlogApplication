f4b671bcf7a43faad25532d06d3eb3bef4acc7ec
Udayraj <udayraj.m@bizamica.com>
2021-07-30 17:05:40 +0530

    Working commit

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="comtent">
+        <Home/>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ dojo-blog/src/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return ( 
+        <div className="Home">
+            <h2>Home Page</h2>
+        </div>
+     );
+}
+ 
+export default Home;
+<div className="Home">
+    <h2>Home Page</h2>
+</div>
